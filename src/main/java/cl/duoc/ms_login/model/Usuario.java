6884a5f8718918a9a71e3f424d0c66b943d0d0e1
César Saavedra C. <ces.saavedra@duocuc.cl>
2026-05-09 13:30:31 -0400

    Cesar
    Primer Commit

---
+++ src/main/java/cl/duoc/ms_login/model/Usuario.java
@@ -0,0 +1,16 @@
+package cl.duoc.ms_login.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    private String id;
+    private String nombre;
+    private String email;
+    private String password;
+}
