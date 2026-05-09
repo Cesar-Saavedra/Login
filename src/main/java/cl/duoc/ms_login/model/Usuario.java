2a7554e4074635df918e7ceb52cbb6a7c689d2ea
César Saavedra C. <ces.saavedra@duocuc.cl>
2026-05-09 17:43:37 -0400

    Cesar
    Avance en Login: Usuario, repo y service

--- src/main/java/cl/duoc/ms_login/model/Usuario.java
+++ src/main/java/cl/duoc/ms_login/model/Usuario.java
@@ -1,5 +1,11 @@
 package cl.duoc.ms_login.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +13,23 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    @Column(nullable = false)
+    private Integer id;
+    
+    @Column(nullable = false)
     private String nombre;
+    
+    @Column(nullable = false)
     private String email;
+    
+    @Column(nullable = false)
     private String password;
     
 }
